f71d263e653846371c3ed8c95a20a32e2f2684e0
René Kooi <rene@kooi.me>
2016-01-25 18:57:06 +0100

    un-overengineer tests

--- test.js
+++ test.js
@@ -9,24 +9,22 @@ function stringifyTitle (o) {
   return o ? '"' + o[0] + '" - "' + o[1] + '"' : 'nothing'
 }
 
-function testFailed (result, expected) {
-  console.error(chalk.red('   ' + error + ' expected ' + stringifyTitle(expected)))
+function testFailed (test, result) {
+  console.error(chalk.red('   ' + error + ' expected ' + stringifyTitle(test.expected)))
   console.error(chalk.red('     but got  ' + stringifyTitle(result)))
 }
-function testSucceeded (result) {
+function testSucceeded (test, result) {
   console.log(chalk.green('   ' + success + ' got ' + stringifyTitle(result)))
 }
 
 function runTest (plugins, test) {
-  var expected = test.slice(1)
-
-  console.log('  ' + test[0])
-  var result = getSongArtistTitle(test[0], plugins)
-  if (!result || result[0] !== expected[0] || result[1] !== expected[1]) {
-    testFailed(result, expected)
+  console.log('  ' + test.input)
+  var result = getSongArtistTitle(test.input, plugins)
+  if (!result || result[0] !== test.expected[0] || result[1] !== test.expected[1]) {
+    testFailed(test, result)
     return false
   }
-  testSucceeded(result)
+  testSucceeded(test, result)
   return true
 }
 
@@ -44,33 +42,19 @@ function runSuite (suite) {
   return score
 }
 
-function readTest (test) {
-  return test.split('\n').filter(function (line) {
-    return line.substr(0, 2) !== '//'
-  })
-}
-
-var PLUGINS = /^plugins: (.*?)$/
-function readSuite (pathName) {
-  var tests = fs.readFileSync(pathName, 'utf8')
-    .split('\n\n')
-
-  var plugins = [ 'base' ]
-  var pluginsLine = tests[0].match(PLUGINS)
-  if (pluginsLine) {
-    plugins = pluginsLine[1].split(',').map(function (str) { return str.trim() })
-    tests.shift()
-  }
+function readSuite (suiteName) {
+  var suite = require('./test/' + suiteName)
   return {
-    tests: tests.map(readTest),
-    plugins: plugins
+    name: suiteName,
+    plugins: suite.plugins,
+    tests: suite.tests
   }
 }
 
 var suites = fs.readdirSync('test')
 var total = { fail: 0, success: 0 }
 suites.forEach(function (suiteName) {
-  var suite = readSuite('test/' + suiteName)
+  var suite = readSuite(suiteName)
 
   var title = suiteName.replace(/\.txt$/, '')
   console.log(title)
---
+++ test/simple.js
@@ -0,0 +1,16 @@
+module.exports = {
+  tests: [
+    // https://youtu.be/JoC3PUBmhFs
+    { input: 'Sum 41 - In Too Deep (Official Video)',
+      expected: [ 'Sum 41', 'In Too Deep' ] },
+    // https://youtu.be/hn4EIv1-uz0
+    { input: 'Boats & BIrds - Gregory & the Hawk',
+      expected: [ 'Boats & BIrds', 'Gregory & the Hawk' ] },
+    // https://youtu.be/fz3jLeDvpu4
+    { input: 'FEMM - PoW! (Music Video)',
+      expected: [ 'FEMM', 'PoW!' ] },
+    // https://youtu.be/ti1W7Zu8j9k
+    { input: 'The Wombats - Anti-D Official Video',
+      expected: [ 'The Wombats', 'Anti-D' ] }
+  ]
+}
