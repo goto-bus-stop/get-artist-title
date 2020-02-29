d954656550a377b3c87330dabd02fe36cb4efdce
Jake Cheek <jake@f-m.fm>
2020-02-29 04:52:48 -0500

    Clean (Full Album) fluff (#24)
    * Clean (Full Album) fluff
    
    * Create tests for fluff
    
    * remove trailing comma

---
+++ test/fluff.js
@@ -0,0 +1,19 @@
+/* eslint-disable object-curly-newline */
+module.exports = {
+  tests: [
+    { input: 'Rush – Moving Pictures (Full Album)',
+      expected: ['Rush', 'Moving Pictures'] },
+    { input: 'Rush - Moving Pictures (album)',
+      expected: ['Rush', 'Moving Pictures'] },
+    { input: 'Rush - Moving Pictures (Official Album)',
+      expected: ['Rush', 'Moving Pictures'] },
+    { input: 'Rush - Moving Pictures (Full Album) (Official)',
+      expected: ['Rush', 'Moving Pictures'] },
+    { input: 'FILMMAKER - ETERNAL RETURN [FULL ALBUM]',
+      expected: ['FILMMAKER', 'ETERNAL RETURN'] },
+    { input: 'Dua Lipa - New Rules (Official Music Video) **NEW**',
+      expected: ['Dua Lipa', 'New Rules'] },
+    { input: 'Muse — The 2nd Law (Full Album) [HD]',
+      expected: ['Muse', 'The 2nd Law'] }
+  ]
+}
